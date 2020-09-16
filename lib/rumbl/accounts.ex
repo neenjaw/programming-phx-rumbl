3357cb824bb6d9f924dd03c26f68c4752c79a859
Tim Austin <tim@neenjaw.com>
2020-09-15 22:24:51 -0600

    pull data from DB

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -3,23 +3,22 @@ defmodule Rumbl.Accounts do
   The Accounts context.
   """
 
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
-  def list_users do
-    [
-      %User{id: "1", name: "Jose", username: "josevalim"},
-      %User{id: "2", name: "Bruce", username: "redrapids"},
-      %User{id: "3", name: "Chris", username: "chrismccord"}
-    ]
+  def list_users() do
+    Repo.all(User)
   end
 
   def get_user(id) do
-    Enum.find(list_users(), fn user -> user.id == id end)
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn user ->
-      Enum.all?(params, fn {key, val} -> Map.get(user, key) == val end)
-    end)
+    Repo.get_by(User, params)
   end
 end
