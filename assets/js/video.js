287c60a825713426ecafa0eba27fa28734446797
Tim Austin <tim@neenjaw.com>
2020-09-24 11:33:29 -0600

    presence implemented

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,4 +1,5 @@
 import Player from "./player";
+import { Presence } from "phoenix";
 
 const Video = {
   init(socket, element) {
@@ -18,9 +19,22 @@ const Video = {
     const msgContainer = document.getElementById("msg-container");
     const msgInput = document.getElementById("msg-input");
     const postButton = document.getElementById("msg-submit");
+    const userList = document.getElementById("user-list");
+
     let lastSeenId = 0;
     const vidChannel = socket.channel("videos:" + videoId, () => {
       return { last_seen_id: lastSeenId };
+    });
+
+    const presence = new Presence(vidChannel);
+
+    presence.onSync(() => {
+      userList.innerHTML = presence
+        .list((id, { user: user, metas: [first, ...rest] }) => {
+          const count = rest.length + 1;
+          return `<li>${user.username}: (${count})</li>`;
+        })
+        .join("");
     });
 
     postButton.addEventListener("click", (e) => {
--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -3,11 +3,17 @@ defmodule Rumbl.Accounts do
   The Accounts context.
   """
 
+  import Ecto.Query
+
   alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
   def list_users() do
     Repo.all(User)
+  end
+
+  def list_users_with_ids(ids) do
+    Repo.all(from u in User, where: u.id in ^ids)
   end
 
   def get_user(id) do
